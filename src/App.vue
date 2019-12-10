9890a28ad755c63900ef7cc7182abfcac6d338bd
weidong <weidongchen.auctionsoftware@gmail.com>
2019-12-10 13:11:30 +0530

    basic of solt

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <app-form>
+      <div slot="form-header">
+        <h3>Form for get the basic infomation.</h3>
+      </div>
+      <div slot="form-field">
+        <input type="text" placeholder="Enter person full name" />
+        <br />
+        <input type="number" placeholder="Enter person Mobile number" />
+        <br />
+        <input type="email" placeholder="Enter person Email" />
+        <br />
+        <input type="password" placeholder="Enter the Password" />
+        <br />
+      </div>
+      <div slot="form-control">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
+    </app-form>
+  </div>
+</template>
+
+<script>
+import formclass from "./component/formclass.vue";
+export default {
+  components: {
+    "app-form": formclass
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    handleSubmit: function() {
+      alert("hello");
+    }
+  }
+};
+</script>
+
+<style>
+input {
+  margin: 5px;
+  padding: 5px;
+  text-align: center;
+}
+div {
+  text-align: center;
+}
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/component/formclass.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <form>
+      <div id="form-header">
+        <slot name="form-header"></slot>
+      </div>
+      <div id="form-field">
+        <slot name="form-field"></slot>
+      </div>
+      <div id="form-control">
+        <slot name="form-control"></slot>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
